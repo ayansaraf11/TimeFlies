2a1a0b0f44cbdaa984719823661334a20067ec0d
ayansaraf11 <ayans2@uw.edu>
2017-11-29 23:41:23 -0800

    Added server and ui file

---
+++ ui.R
@@ -0,0 +1,16 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("TimeFlies"),
+  
+  sidebarLayout(
+    sidebarPanel(),
+    
+    mainPanel(
+    )
+  )
+))
